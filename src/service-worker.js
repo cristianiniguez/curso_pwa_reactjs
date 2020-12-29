083a35ad6c13b96e531e4304254e54e3fe98eaac
criniguez <felizpc1@gmail.com>
2020-12-29 11:11:20 -0400

    service workers: introduccion a workbox

---
+++ src/service-worker.js
@@ -0,0 +1,8 @@
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+
+self.__precacheManifest = [].concat(self.__precacheManifest || [], self.__WB_MANIFEST || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
