a41d592bd1633ca0d47eb918539b65a5c9f7a27a
criniguez <felizpc1@gmail.com>
2020-12-29 12:34:24 -0400

    service workers: aplicando estrategias de carga

--- src/service-worker.js
+++ src/service-worker.js
@@ -8,4 +8,23 @@ self.__precacheManifest = [].concat(self.__precacheManifest || [], self.__WB_MAN
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute('/index.html');
+
+// Google Fonts Caching
+workbox.routing.registerRoute(
+  /^https:\/\/fonts.(?:googleapis|gstatic).com\/(.*)/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'google-fonts-cache',
+    plugins: [new workbox.expiration.Plugin({ maxAgeSeconds: 30 * 24 * 3600 })],
+  }),
+  'GET',
+);
+
+// Api request Caching
+workbox.routing.registerRoute(
+  /^https?:\/\/www.themealdb.com\/api\/.*/,
+  new workbox.strategies.StaleWhileRevalidate(),
+  'GET',
+);
+
+// Default Caching
 workbox.routing.registerRoute(/^https?.*/, new workbox.strategies.NetworkFirst(), 'GET');
