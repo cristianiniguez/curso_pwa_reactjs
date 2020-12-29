d5f5772c0607e8d64f940c99f282a38e0869be5b
criniguez <felizpc1@gmail.com>
2020-12-29 12:04:52 -0400

    service workers: implementando workbox

--- src/service-worker.js
+++ src/service-worker.js
@@ -6,3 +6,6 @@
 
 self.__precacheManifest = [].concat(self.__precacheManifest || [], self.__WB_MANIFEST || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute('/index.html');
+workbox.routing.registerRoute(/^https?.*/, new workbox.strategies.NetworkFirst(), 'GET');
